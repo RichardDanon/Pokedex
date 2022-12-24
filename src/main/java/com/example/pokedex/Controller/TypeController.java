4adee287811d3937d6c858a3872af17a1d5d8512
RichardDanon <104465518+RichardDanon@users.noreply.github.com>
2022-12-23 19:26:45 -0500

    Get Request for Types Complete

--- src/main/java/com/example/pokedex/Controller/TypeController.java
+++ src/main/java/com/example/pokedex/Controller/TypeController.java
@@ -1,10 +1,13 @@
 package com.example.pokedex.Controller;
 
-import com.example.pokedex.Service.RegionService;
+import com.example.pokedex.Entity.Region;
+import com.example.pokedex.Entity.Type;
+import com.example.pokedex.Response.*;
 import com.example.pokedex.Service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/types")
@@ -12,4 +15,18 @@ public class TypeController {
 
     @Autowired
     TypeService typeService;
+
+    @GetMapping
+    public List<TypeResponse> getAllTypes(){
+
+        List<Type> types = typeService.getAllTypes();
+        List<TypeResponse> typeResponses = new ArrayList<>();
+
+        types.forEach(type -> {
+            TypeResponse typeResponse = new TypeResponse(type);
+            typeResponses.add(typeResponse);
+        });
+
+        return typeResponses;
+    }
 }
